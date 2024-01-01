f361b52e581b4b36589d6c8dc8850b39b6af9322
pourya90091 <pourya90091@gmail.com>
2024-01-01 15:38:56 +0330

    file: Add templates/index.html

--- pourya90091/urls.py
+++ pourya90091/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from pourya90091 import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.index, name='index'),
     path("accounts/", include("django.contrib.auth.urls")),
 ]
