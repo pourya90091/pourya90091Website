38e56c525881a11f717f307f06ead0a57f8da0d9
pourya90091 <pourya90091@gmail.com>
2024-01-02 19:35:13 +0330

    implementation: Post method logic was implemented in SignUpView and LoginView

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,9 +1,7 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm
-from accounts.models import User
 
 
-class SignUpForm(UserCreationForm):
+class SignUpForm(forms.Form):
     username = forms.CharField(max_length=50, required=True)
     password = forms.CharField(max_length=80, required=True)
     confirm_password = forms.CharField(max_length=80, required=True)
--- accounts/views.py
+++ accounts/views.py
@@ -13,19 +13,59 @@ User = get_user_model()
 
 class SignUpView(View):
     def get(self, req: HttpRequest):
-        return render(req, 'accounts/signup.html', {
-            'signup_form': SignUpForm()
+        return render(req, "accounts/signup.html", {
+            "signup_form": SignUpForm()
         })
 
     def post(self, req: HttpRequest):
-        pass
+        signup_form = SignUpForm(req.POST)
+        if signup_form.is_valid():
+            username = signup_form.cleaned_data.get("username")
+            password = signup_form.cleaned_data.get("password")
+            confirm_password = signup_form.cleaned_data.get("confirm_password")
+            email = signup_form.cleaned_data.get("email")
+
+            user_exists = User.objects.filter(username__iexact=username).exists()
+            if user_exists:
+                signup_form.add_error("username", "Username is already taken.")
+
+            if not password == confirm_password:
+                signup_form.add_error("password", "Password not confirmed correctly.")
+            else:
+                new_user = User(username=username,
+                                email=email)
+                new_user.set_password(password)
+                new_user.save()
+
+                return redirect(reverse("index"))
+
+        return render(req, "accounts/signup.html", {
+            "signup_form": signup_form
+        })
 
 
 class LoginView(View):
     def get(self, req: HttpRequest):
-        return render(req, 'accounts/login.html', {
-            'login_form': LoginForm()
+        return render(req, "accounts/login.html", {
+            "login_form": LoginForm()
         })
 
     def post(self, req: HttpRequest):
-        pass
+        login_form = LoginForm(req.POST)
+        if login_form.is_valid():
+            username = login_form.cleaned_data.get("username")
+            password = login_form.cleaned_data.get("password")
+
+            user = User.objects.filter(username__iexact=username).first()
+            if user:
+                if user.check_password(password):
+                    login(req, user)
+                    return redirect(reverse("index"))
+                else:
+                    login_form.add_error("password", "Password is not correct")
+            else:
+                login_form.add_error("username", "There is no user with that username.")
+
+        return render(req, "accounts/login.html", {
+            "login_form": login_form
+        })
