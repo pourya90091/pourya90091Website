a5c1d1967ab20b7ccf4271963864be4716adc2ac
pourya90091 <pourya90091@gmail.com>
2024-01-05 21:12:41 +0330

    refactor: Minor changes in accounts/views.py

--- accounts/views.py
+++ accounts/views.py
@@ -18,6 +18,20 @@ class SignUpView(View):
         })
 
     def post(self, req: HttpRequest):
+        def data_validation(username):
+            data_is_valid = True
+
+            user_exists = User.objects.filter(username__iexact=username).exists()
+            if user_exists:
+                signup_form.add_error("username", "Username is already taken.")
+                data_is_valid = False
+
+            if not password == confirm_password:
+                signup_form.add_error("password", "Password not confirmed correctly.")
+                data_is_valid = False
+
+            return data_is_valid
+
         signup_form = SignUpForm(req.POST)
         if signup_form.is_valid():
             username = signup_form.cleaned_data.get("username")
@@ -25,13 +39,8 @@ class SignUpView(View):
             confirm_password = signup_form.cleaned_data.get("confirm_password")
             email = signup_form.cleaned_data.get("email")
 
-            user_exists = User.objects.filter(username__iexact=username).exists()
-            if user_exists:
-                signup_form.add_error("username", "Username is already taken.")
-
-            if not password == confirm_password:
-                signup_form.add_error("password", "Password not confirmed correctly.")
-            else:
+            data_is_valid = data_validation(username)
+            if data_is_valid:
                 new_user = User(username=username,
                                 email=email)
                 new_user.set_password(password)
@@ -51,20 +60,30 @@ class LoginView(View):
         })
 
     def post(self, req: HttpRequest):
+        def data_validation():
+            data_is_valid = True
+
+            if not user:
+                login_form.add_error("username", "There is no user with that username.")
+                data_is_valid = False
+
+            if not user.check_password(password):
+                login_form.add_error("password", "Password is not correct")
+                data_is_valid = False
+
+            return data_is_valid
+
         login_form = LoginForm(req.POST)
         if login_form.is_valid():
             username = login_form.cleaned_data.get("username")
             password = login_form.cleaned_data.get("password")
 
             user = User.objects.filter(username__iexact=username).first()
-            if user:
-                if user.check_password(password):
-                    login(req, user)
-                    return redirect(reverse("index"))
-                else:
-                    login_form.add_error("password", "Password is not correct")
-            else:
-                login_form.add_error("username", "There is no user with that username.")
+
+            data_is_valid = data_validation()
+            if data_is_valid:
+                login(req, user)
+                return redirect(reverse("index"))
 
         return render(req, "accounts/login.html", {
             "login_form": login_form
