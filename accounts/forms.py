a4920de182b7392dd6da3c5be0113578a16d1699
pourya90091 <pourya90091@gmail.com>
2024-01-05 21:25:05 +0330

    refactor: Add novalidate attribute to login and signup forms in templates/accounts + Minor changes in accounts/forms.py

--- accounts/forms.py
+++ accounts/forms.py
@@ -2,12 +2,12 @@ from django import forms
 
 
 class SignUpForm(forms.Form):
-    username = forms.CharField(max_length=50, required=True)
-    password = forms.CharField(max_length=80, required=True)
-    confirm_password = forms.CharField(max_length=80, required=True)
-    email = forms.EmailField(max_length=100)
+    username = forms.CharField(max_length=64, required=True)
+    password = forms.CharField(max_length=128, required=True)
+    confirm_password = forms.CharField(max_length=128, required=True)
+    email = forms.EmailField(max_length=128, required=False)
 
 
 class LoginForm(forms.Form):
-    username = forms.CharField(max_length=50, required=True)
-    password = forms.CharField(max_length=80, required=True)
+    username = forms.CharField(max_length=64, required=True)
+    password = forms.CharField(max_length=128, required=True)
