e31f8152262fb28bc79f10bd02b5321c35a7fa6e
pourya90091 <pourya90091@gmail.com>
2024-01-05 21:43:37 +0330

    improvement: Making index page dynamic in showing login and signup page (templates/index.html)

--- pourya90091/views.py
+++ pourya90091/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from django.urls import reverse
 
 
 def index(req):
-    return render(req, 'index.html', {})
+    return render(req, "index.html", {
+        "login_url": reverse("login"),
+        "signup_url": reverse("signup")
+        })
