2c407aad3c65071531102bb53e2cc2e18d3b65d3
pourya90091 <pourya90091@gmail.com>
2024-01-12 19:34:21 +0330

    file: Add utils/email.py + Configure EMAIL settings

--- pourya90091/settings.py
+++ pourya90091/settings.py
@@ -124,3 +124,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = '' # An email for use by the application
+EMAIL_HOST_PASSWORD = '' # Password of the application email
+EMAIL_PORT = 587
