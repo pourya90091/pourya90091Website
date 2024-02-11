ca0ec7f1494433f8f66f4045a3994899f14eb9ea
pourya90091 <pourya90091@gmail.com>
2024-02-11 19:05:52 +0330

    implement: implement ArticleView in article app

--- article/views.py
+++ article/views.py
@@ -1,19 +1,31 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth import get_user_model
 from django.utils.decorators import method_decorator
 from django.urls import reverse
 from django.views import View
-from django.http import HttpRequest
+from django.http import HttpRequest, Http404
 from article.forms import ArticleForm
 from article.models import Article
+import markdown
+
+
+User = get_user_model()
 
 
 class ArticleView(View):
-    def get(self, req: HttpRequest, slug):
-        pass
+    def get(self, req: HttpRequest, username, slug):
+        user = User.objects.filter(username__exact=username).first()
+        article_exists = Article.objects.filter(slug__iexact=slug, user__exact=user).exists()
+        if article_exists:
+            article = Article.objects.filter(slug__iexact=slug, user__exact=user).first()
+            md = markdown.Markdown(extensions=["fenced_code"])
+            article.content = md.convert(article.content)
+            return render(req, "article/article.html", {
+                "article": article
+            })
 
-    def post(self, req: HttpRequest, slug):
-        pass
+        raise Http404()
 
 
 @method_decorator(login_required, name='dispatch')
